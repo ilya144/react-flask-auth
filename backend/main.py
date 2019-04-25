183d32f748424556c32ec2ddbaebd11a49b25a66
ilya144 <ils12rus@gmail.com>
2019-04-25 15:12:34 +0300

    small changes

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
+import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'notsosecret' # key for JWT signature
 
-@app.route('/login')
+@app.route('/login', method=['POST'])
 def login():
     pass
 
-@app.route('/register')
+@app.route('/register', method=['POST'])
 def register():
     pass
 
-
+if __name__ == "__main__":
+    pass
